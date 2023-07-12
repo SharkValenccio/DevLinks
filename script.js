462a41a09f9b6810a8b01dc88604035ad49aa224
SharkValenccio <sharkvalenccio@gmail.com>
2023-07-12 11:16:04 -0300

    JS change profile img code name

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function togglemode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
-  } else {
     img.setAttribute("src", "./assets/avatar.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar-light.png")
   }
 
   // IF e ELSE funcionando no codigo
